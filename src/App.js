8b263a2d28c72c4df22982bd2e01ff90c204d372
manjirimardikar <manjari.mardikar28@gmail.com>
2023-04-28 19:53:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from './Components/Task';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{maxHeight:'100vh',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column',gap:'30px'}}>
+      <div>
+        <h1 style={{color:'peachpuff',fontSize:'4rem'}}>Todo APP</h1>
+        
+      </div>
+      <Task/>
     </div>
   );
 }
